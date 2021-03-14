f7f333c137ab9d719175f3ac4c6ba8d8417d1b58
cyanorca <inboxtohana+sourcetree@gmail.com>
2021-03-15 03:38:50 +1100

    made page indicator work y aaaayayayayy

--- lib/v2/ui/modal_widgets/slider_widget.dart
+++ lib/v2/ui/modal_widgets/slider_widget.dart
@@ -4,72 +4,75 @@ import 'package:coinsnap/v2/ui/modal_widgets/link_api_helper_modal.dart';
 import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
-class CarouselDemo extends StatelessWidget {
+class CarouselDemo extends StatefulWidget {
+  @override
+  _CarouselDemoState createState() => _CarouselDemoState();
+}
+
+class _CarouselDemoState extends State<CarouselDemo> {
   final CarouselController buttonCarouselController = CarouselController();
 
   @override
   Widget build(BuildContext context) {
-
     // total number of pages on this slider
-    List<int> pageList = [1,2,3,4,5,6,7];
+    List<int> pageList = [1, 2, 3, 4, 5, 6, 7];
+
+    // current page index used for page indicator
+    int _curr = 0;
 
     return Scaffold(
-      body:SafeArea(
+      body: SafeArea(
         child: Container(
-        
-        // background gradient
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment(-1.08, -1.08),
-            end: Alignment(1, 1.06),
-            colors: [
-              Color(0xFF443E48),
-              Color(0xFF1B1F2D),
-            ],
-          ),
-          // without this it has a thin grey border for some reason D:<
-          border: Border.all(width: 0)
-        ),
-        
-        child: Stack(
-          children: [
-            Column(
-              children: [
-                Expanded(
-                  child: ListView(
-                    children: [
+          // background gradient
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color(0xFF383640),
+                  Color(0xFF282831),
+                ],
+              ),
+              // without this it has a thin grey border for some reason D:<
+              border: Border.all(width: 0)),
+
+          child: Stack(children: [
+            StatefulBuilder(builder: (context, setState) {
+              return Column(
+                children: [
+                  Expanded(
+                    child: ListView(children: [
                       CarouselSlider(
                         // iterate through the list of page numbers to generate each page
                         items: pageList.map((p) {
-                            return Container(
-                              margin: EdgeInsets.all(5.0),
-                              child: LinkAPIHelperModal(page: p),
-                            );
-                          }
-                        ).toList(),
+                          return Container(
+                            margin: EdgeInsets.all(5.0),
+                            child: LinkAPIHelperModal(page: p),
+                          );
+                        }).toList(),
 
                         //Slider Container properties
                         options: CarouselOptions(
-                          height: displayHeight(context) * 0.8,
-                          aspectRatio: 16 / 9,
-                          viewportFraction: 1,
-                          initialPage: 0,
-                          enableInfiniteScroll: false,
-                        ),
+                            height: displayHeight(context) * 0.8,
+                            aspectRatio: 16 / 9,
+                            viewportFraction: 1,
+                            initialPage: 0,
+                            enableInfiniteScroll: false,
+                            onPageChanged: (index, reason) {
+                              setState(() => _curr = index);
+                            }),
                       ),
-                    ],
+                    ]),
                   ),
-                ),
-                Container (
-                  height: 100,
-                  // Smooth page indicator
-                  child: PageIndicator(1)
-                ),
-              ],
-            )
-          ]
+                  Container(
+                      height: 100,
+                      // Smooth page indicator
+                      child: PageIndicator(_curr)),
+                ],
+              );
+            }),
+          ]),
         ),
-      ),
       ),
     );
   }
@@ -88,14 +91,13 @@ class PageIndicator extends StatelessWidget {
       child: AnimatedSmoothIndicator(
         activeIndex: page,
         count: 7,
-      effect: WormEffect(
-        paintStyle: PaintingStyle.stroke,
-        dotWidth: 6,
-        dotHeight: 6,
-        spacing: 15,
-        dotColor: Colors.deepPurpleAccent,
-        activeDotColor: Colors.deepPurple[100]
-      ),
+        effect: WormEffect(
+            paintStyle: PaintingStyle.stroke,
+            dotWidth: 6,
+            dotHeight: 6,
+            spacing: 15,
+            dotColor: Colors.deepPurple,
+            activeDotColor: Colors.deepPurpleAccent),
       ),
     );
   }
