3b1964546a54495402b21f2670462f8b3b7d0260
cyanorca <inboxtohana+sourcetree@gmail.com>
2021-04-29 23:45:39 +1000

    made trapeziums

--- lib/modules/portfolio_builder/pages/portfolio_builder_select.dart
+++ lib/modules/portfolio_builder/pages/portfolio_builder_select.dart
@@ -1,9 +1,10 @@
+import 'package:coinsnap/modules/utils/sizes_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:coinsnap/modules/utils/colors_helper.dart';
 
 
 class PortfolioBuilderSelect extends StatefulWidget {
-  PortfolioBuilderSelect({Key key}) : super(key: key);
+  PortfolioBuilderSelect();
 
   @override
   _PortfolioBuilderSelectState createState() => _PortfolioBuilderSelectState();
@@ -22,10 +23,135 @@ class _PortfolioBuilderSelectState extends State<PortfolioBuilderSelect> {
         appBar: AppBar(
           title: Text('Build new Portfolio'),
         ),
-        body: Center(
-          child: Text('asdfasf'),
-        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ClipPath(
+              clipper: TrapeziumClipperRight(),
+              child: quickBuildButton(),
+            ), 
+            ClipPath(
+              clipper: TrapeziumClipperLeft(),
+              child: defaultBuildButton(),
+            ),
+          ],
+        )
       ),
     );
   }
+
+  quickBuildButton() {
+    return Card(
+      // decoration: BoxDecoration(
+      //   color: Colors.blue,
+      //   boxShadow: [
+      //     BoxShadow(
+      //       color: Colors.red,
+      //       blurRadius: 2.0,
+      //       spreadRadius: 0.0,
+      //       offset: Offset(2.0, 2.0),
+      //     ),
+      //   ]
+      // ),
+      // height: 200,
+      // width: displayWidth(context),
+      
+      color: Colors.blue,
+      child: InkWell(
+        splashColor: Colors.purple.withAlpha(30),
+        onTap: () {
+          print('quickbuild card tapped');
+        },
+
+        child: SizedBox(
+          height: 200,
+          child: Stack(
+            children: [
+              Positioned(
+                right:0,
+                child: Container(
+                  width: 200,
+                  height: 200,
+                  color: Colors.indigo,
+                  child: Text("image goes here"),
+                ),
+              ),
+              Container(
+                child: Row(
+                  children: [
+                    Flexible(
+                      flex: 2,
+                      child: Text('Quick Build')
+                    ),  
+                    Flexible(
+                      flex: 1,
+                      child: Text('Fast, easy, no frills.')
+                    ), 
+                  ],
+                ),
+              ),
+
+          ],),
+        ),
+      ), 
+    );
+  }
+  
+  defaultBuildButton() {
+    return Container(
+      color: Colors.blue,
+      height: 200,
+      width: displayWidth(context),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Flexible(
+            flex: 2,
+            child: Text('bleh')
+          ),  
+          Flexible(
+            flex: 1,
+            child: Text('blaaah')
+          ),  
+        ],
+      ), 
+    );
+  }
+
 }
+
+class TrapeziumClipperRight extends CustomClipper<Path>{
+  @override
+  Path getClip(Size size) {
+    // trapezium
+    Path path = Path()
+    ..lineTo(size.width, 0)
+    ..lineTo(size.width * 0.85, size.height)
+    ..lineTo(0, size.height)
+    ..close();
+    return path;
+  }
+  
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => false;
+
+}
+
+class TrapeziumClipperLeft extends CustomClipper<Path>{
+  @override
+  Path getClip(Size size) {
+    // trapezium
+    Path path = Path()
+    ..lineTo(size.width, 0)
+    ..lineTo(size.width, size.height)
+    ..lineTo(size.width * 0.15, size.height)
+    ..close();
+    return path;
+  }
+  
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => false;
+
+}
+
