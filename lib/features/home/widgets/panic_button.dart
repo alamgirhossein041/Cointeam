410edb4ca7ce2dae796af07be6c121c3603afaf8
cyanorca <inboxtohana+sourcetree@gmail.com>
2021-06-29 21:33:22 +1000

    dotted circles!

--- lib/features/home/widgets/panic_button.dart
+++ lib/features/home/widgets/panic_button.dart
@@ -16,55 +16,100 @@ class PanicButton extends StatefulWidget {
 class PanicButtonState extends State<PanicButton> {
   @override
   Widget build(BuildContext context) {
+    Widget _greenCircle = Container(
+      width: 118.0,
+      height: 118.0,
+      decoration: new BoxDecoration(
+        color: primaryGreen,
+        shape: BoxShape.circle,
+      ),
+    );
+    // The actual button being pressed. Has the bolt icon.
+    Widget _boltButton = Container(
+      width: 90,
+      height: 90,
+      child: ElevatedButton(
+        child: SvgPicture.asset('graphics/assets/svg/bolt_green.svg'),
+        style: ElevatedButton.styleFrom(
+          shape: CircleBorder(),
+          primary: primaryLight,
+        ),
+        onPressed: () => {
+          BlocProvider.of<StartupBloc>(context).add(FetchStartupEvent()),
+          Navigator.pushNamed(context, '/sellportfolio'),
+        },
+      ),
+    );
+    // Here begins the series of dotted circles lol
+    Widget _dottedCircle1 = Container(
+      width: 128,
+      height: 128,
+      child: DottedBorder(
+        borderType: BorderType.Circle,
+        color: primaryGreen.withOpacity(0.9),
+        strokeWidth: 2.5,
+        dashPattern: [0, 6.8],
+        strokeCap: StrokeCap.round,
+        child: Container(
+          width: 4,
+          height: 4,
+        ),
+      ),
+    );
+    Widget _dottedCircle2 = Container(
+      width: 142,
+      height: 142,
+      child: DottedBorder(
+        borderType: BorderType.Circle,
+        color: primaryGreen.withOpacity(0.5),
+        strokeWidth: 2.5,
+        dashPattern: [0, 9.1],
+        strokeCap: StrokeCap.round,
+        child: Container(
+          width: 4,
+          height: 4,
+        ),
+      ),
+    );
+    Widget _dottedCircle3 = Container(
+      width: 158,
+      height: 158,
+      child: DottedBorder(
+        borderType: BorderType.Circle,
+        color: primaryGreen.withOpacity(0.3),
+        strokeWidth: 2.5,
+        dashPattern: [0, 14.7],
+        strokeCap: StrokeCap.round,
+        child: Container(
+          width: 4,
+          height: 4,
+        ),
+      ),
+    );
+    Widget _dottedCircle4 = Container(
+      width: 178,
+      height: 178,
+      child: DottedBorder(
+        borderType: BorderType.Circle,
+        color: primaryGreen.withOpacity(0.2),
+        strokeWidth: 2.5,
+        dashPattern: [0, 18],
+        strokeCap: StrokeCap.round,
+        child: Container(
+          width: 4,
+          height: 4,
+        ),
+      ),
+    );
     return Stack(
       alignment: AlignmentDirectional.center,
       children: [
-        Container(
-          width: 118.0,
-          height: 118.0,
-          decoration: new BoxDecoration(
-            color: primaryGreen,
-            shape: BoxShape.circle,
-          ),
-        ),
-        // Container(
-        //   width: 90,
-        //   height: 90,
-        //   decoration: new BoxDecoration(
-        //     color: primaryLight,
-        //     shape: BoxShape.circle,
-        //   ),
-        // ),
-        Container(
-          width: 90,
-          height: 90,
-          child: ElevatedButton(
-            child: SvgPicture.asset('graphics/assets/svg/bolt_green.svg'),
-            style: ElevatedButton.styleFrom(
-              shape: CircleBorder(),
-              primary: primaryLight,
-            ),
-            onPressed: () => {
-              BlocProvider.of<StartupBloc>(context).add(FetchStartupEvent()),
-              Navigator.pushNamed(context, '/sellportfolio'),
-            },
-          ),
-        ),
-        Container(
-          width: 128,
-          height: 128,
-          child: DottedBorder(
-            borderType: BorderType.Circle,
-            color: primaryGreen,
-            strokeWidth: 2,
-            dashPattern: [0, 8.2],
-            strokeCap: StrokeCap.round,
-            child: Container(
-              width: 4,
-              height: 4,
-            ),
-          ),
-        ),
+        _greenCircle,
+        _boltButton,
+        _dottedCircle1,
+        _dottedCircle2,
+        _dottedCircle3,
+        _dottedCircle4,
       ],
     );
   }
