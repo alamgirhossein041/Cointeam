1c9872fc7da388d9c6f0edca5c07f7723de3bd54
Andrew Cho <andrew.cho1992@gmail.com>
2021-07-02 10:16:08 +1000

    updated home fields with bloc data (except market cap)

--- lib/features/home/pages/home.dart
+++ lib/features/home/pages/home.dart
@@ -1,3 +1,5 @@
+import 'package:coinsnap/features/data/global_stats/coingecko/bloc/gecko_global_stats_bloc.dart';
+import 'package:coinsnap/features/data/global_stats/global_stats.dart';
 import 'package:coinsnap/features/home/widgets/home_menu_button.dart';
 import 'package:coinsnap/features/utils/sizes_helper.dart';
 import 'package:flutter/material.dart';
@@ -31,6 +33,7 @@ class HomeState extends State<Home> {
   void initState() {
     super.initState();
     BlocProvider.of<StartupBloc>(context).add(FetchStartupEvent());
+    BlocProvider.of<GeckoGlobalStatsBloc>(context).add(GeckoGlobalStatsFetchEvent());
     helloWorld = BinanceTimeSyncRepositoryImpl();
     helloWorld.getBinanceTimeSyncLatest();
   }
--- lib/features/home/widgets/dominance.dart
+++ lib/features/home/widgets/dominance.dart
@@ -1,4 +1,9 @@
+import 'dart:developer';
+
+import 'package:coinsnap/features/data/global_stats/global_stats.dart';
+import 'package:coinsnap/features/widget_templates/loading_error_screens.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class DominanceWidget extends StatefulWidget {
 
@@ -10,16 +15,43 @@ class _DominanceWidgetState extends State<DominanceWidget> {
   @override
   Widget build(BuildContext context) {
     return Container(
-       child: Column(
+      child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-         children: [
-           Text('Dominance'),
-           Row(children: [
-             Text('BTC: 64%%'),
-             Text('ETH: 12%'),
-           ],)
-         ],
-       ),
+        children: <Widget> [
+          Text('Dominance'),
+          /// Bloc Coingecko info retrieval
+          BlocConsumer<GeckoGlobalStatsBloc, GeckoGlobalStatsState>(
+            listener: (context, state) {
+              if (state is GeckoGlobalStatsErrorState) {
+                log("Error in dominance.dart _DominanceWidgetState");
+              }
+            },
+            builder: (context, state) {
+              if (state is GeckoGlobalStatsLoadedState) {
+                return Row(
+                  children: <Widget> [
+                    Text('BTC: ' + state.geckoGlobalStats.totalMarketCapPct['btc'].toStringAsFixed(0) + '%'),
+                    Text('  ETH: ' + state.geckoGlobalStats.totalMarketCapPct['eth'].toStringAsFixed(0) + '%'),
+                  ]
+                );
+              } else if (state is GeckoGlobalStatsErrorState) {
+                return Text("An error has occured, CoinGecko-related.");
+              } else {
+                return Row(
+                  children: <Widget> [
+                    Text('BTC: ' ),
+                    loadingTemplateWidget(16,2),
+                    Text('  '),
+                    Text('ETH:  '),
+                    loadingTemplateWidget(16,2),
+                  ]
+                );
+              }
+            }
+          )
+          /// End bloc
+        ],
+      ),
     );
   }
 }
