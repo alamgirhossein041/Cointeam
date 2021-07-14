863bcea46707143b3aafc349de66a7fa4b6ce96a
cyanorca <inboxtohana+sourcetree@gmail.com>
2021-07-14 18:32:47 +1000

    using dummy map

--- lib/features/snapshots/pages/snapshot_list.dart
+++ lib/features/snapshots/pages/snapshot_list.dart
@@ -219,6 +219,114 @@ class SnapshotListState extends State<SnapshotList> {
   }
 
   Future getStorage() async {
+    List<dynamic> dummyMap = [
+      {
+        "coins": {
+          "BNB": {"quantity": 95.2, "value": 12.569256},
+          "NEO": {"quantity": 95.2, "value": 12.569256},
+          "TRX": {"quantity": 95.2, "value": 12.569256}
+        },
+        "currency": "USDT",
+        "total": 45.651713,
+        "timestamp": 1620738580285
+      },
+      {
+        "coins": {
+          "USDT": {"quantity": 0.000799, "value": 45.7543355}
+        },
+        "currency": "BTC",
+        "total": 45.7543355,
+        "timestamp": 1620798793883
+      },
+      {
+        "coins": {
+          "USDT": {"quantity": 0.000406, "value": 23.14507342}
+        },
+        "currency": "BTC",
+        "total": 23.14507342,
+        "timestamp": 1620805525045
+      },
+      {
+        "coins": {
+          "SHIB": {"quantity": 744937.0, "value": 21.55847678}
+        },
+        "currency": "USDT",
+        "total": 21.55847678,
+        "timestamp": 1620813201918
+      },
+      {
+        "coins": {
+          "XRP": {"quantity": 662213.0, "value": 19.22404339},
+          "SHIB": {"quantity": 662213.0, "value": 19.22404339}
+        },
+        "currency": "USDT",
+        "total": 43.18642339,
+        "timestamp": 1620813331191
+      },
+      {
+        "coins": {
+          "XRP": {"quantity": 14.48, "value": 21.19872},
+          "SHIB": {"quantity": 772092.0, "value": 22.44471444}
+        },
+        "currency": "USDT",
+        "total": 43.64343444,
+        "timestamp": 1620814453936
+      },
+      {
+        "coins": {
+          "BNB": {"quantity": 0.3683, "value": 136.447784}
+        },
+        "currency": "USDT",
+        "total": 136.447784,
+        "timestamp": 1623754999095
+      },
+      {
+        "coins": {
+          "BTC": {"quantity": 0.037891, "value": 1127.35841897}
+        },
+        "currency": "USDT",
+        "total": 1127.35841897,
+        "timestamp": 1624371972665
+      },
+      {
+        "coins": {},
+        "currency": "USDT",
+        "total": 0.0,
+        "timestamp": 1624934928986
+      },
+      {
+        "coins": {},
+        "currency": "USDT",
+        "total": 0.0,
+        "timestamp": 1624934928986
+      },
+      {
+        "coins": {
+          "ETH": {"quantity": 0.24144, "value": 505.1793984},
+          "MATIC": {"quantity": 449.0, "value": 505.352027},
+          "BTC": {"quantity": 0.014669, "value": 503.78523806},
+          "AUD": {"quantity": 659.2, "value": 498.0256}
+        },
+        "currency": "USDT",
+        "total": 2012.34226346,
+        "timestamp": 1624935141451
+      },
+      {
+        "coins": {
+          "USDT": {"quantity": 141.19140915, "value": 0.004023}
+        },
+        "currency": "BTC",
+        "total": 141.19140915,
+        "timestamp": 1624958729880
+      },
+      {
+        "coins": {},
+        "currency": "USDT",
+        "total": 0.0,
+        "timestamp": 1625625851453
+      }
+    ];
+
     var ready = await localStorage.ready;
     // await localStorage.setItem("portfolio", customVariableHere);
     var value = localStorage.getItem("portfolio");
